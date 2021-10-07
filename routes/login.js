9302962831df57b4e949df95e8c944c8a6247589
Guigas <61693858+AlvarezGui@users.noreply.github.com>
2021-10-07 11:56:19 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,20 @@
 module.exports = function(app){
+    var bcrypt = require('bcrypt');
+    
     app.get('/login', (req, res)=>{
         res.render('login.ejs');
+    });
+
+    app.post('/login', async(req, res)=>{
+        var conexao = require('../config/bancodedados')();
+        var usuarios = require('../models/usuarios');
+
+        var userexiste = await usuarios.findOne({email:req.body.email});
+        if(userexiste){
+            var verificarSenhas = await bcrypt.compare(req.body.senha, userexiste.senha);
+            if(verificarSenhas) res.send('usuario e senha corretos');
+            else res.render('login.ejs', {mensagem:'Senha incorreta'});
+        }
+        else res.render('login.ejs', {mensagem:'Usuario não cadastrado'});
     });
 }
--- routes/registro.js
+++ routes/registro.js
@@ -6,15 +6,17 @@ module.exports = function(app){
     });
 
     app.post('/registro', async(req, res)=>{
-        console.log(req.body);
         var conexao = require('../config/bancodedados')();
         var usuarios = require('../models/usuarios');
 
+        //checar se o email já está cadastrado
         var userexiste = await usuarios.findOne({email:req.body.email});
         if(userexiste){
-            return res.send("Email Já cadastrado");
-        } else{
-            var senhasegura = await bcrypt.hash(req.body.senha, 12);
+            return res.render('registro.ejs', {mensagem:'email já cadastrado'});
+        }
+        //se email não estiver cadastrado, cadastrar
+         else{
+            var senhasegura = await bcrypt.hash(req.body.senha, 12); //criptografar senha
             var documento = new usuarios({
                 nome:req.body.nome,
                 email:req.body.email,
