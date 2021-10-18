63be2f54cf0c75618da4be2fee156ef9cb3a047e
Guigas <61693858+AlvarezGui@users.noreply.github.com>
2021-10-18 11:50:43 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-    var bcrypt = require('bcryptjsww');
+    var bcrypt = require('bcryptjs');
     
     app.get('/login', (req, res)=>{
         res.render('login.ejs');
